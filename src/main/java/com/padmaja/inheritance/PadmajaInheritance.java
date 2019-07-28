8a1453d1b4e79499639dfc8cccdbe9daab1cd96a
unknown <raje1reddy@gmail.com>
2019-07-28 08:52:59 +0100

    Sample code for Inheritance

--- src/main/java/com/padmaja/inheritance/PadmajaInheritance.java
+++ src/main/java/com/padmaja/inheritance/PadmajaInheritance.java
@@ -1,27 +1,10 @@
-package com.padmaja.stringreverse;
+package com.padmaja.inheritance;
 //import static java.lang.System.out;
 import java.util.Scanner;
-import static java.lang.System.out;
 import java.lang.String;
 
-public class StringReverse {
-    public static void main(String[] args){
-        System.out.println("Enter string to reverse:");
-
-        Scanner read = new Scanner(System.in);
-        String str = read.nextLine();
-
-        //creating object of String Util class
-        stringUtil myStringUtilObject = new stringUtil();
-        //called the reverse fuction using the object
-        String reverse = myStringUtilObject.reverse(str);
-
-        System.out.println("Reversed string is:" + reverse);
-
-    }
-
+public class PadmajaInheritance {
 }
-
 
 
 
